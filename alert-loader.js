c4196c5f3e0db513765c733ef8a976029b683ce6
David Perez <dperez19279@columbushs.com>
2018-05-03 11:49:41 -0400

    Update alert-loader.js

--- alert-loader.js
+++ alert-loader.js
@@ -1,1 +1,1 @@
-javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://raw.githubusercontent.com/Dperez19279/Bookkmarklets/master/default-bookmarklet-loader.js';})();
+javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://raw.githubusercontent.com/Dperez19279/Bookkmarklets/master/alert-loader.js';})();
