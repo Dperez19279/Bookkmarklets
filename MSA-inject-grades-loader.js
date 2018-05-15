0d705af232c17de42be86b17406f6ca9ed7eb691
jparrondo24 <justinparrondo@gmail.com>
2018-05-15 17:48:08 -0400

    Add grades injector for Progress page (#1)
    Create MSA-inject-grades.js and MSA-inject-grades-loader.js

---
+++ MSA-inject-grades-loader.js
@@ -0,0 +1,1 @@
+javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://dperez19279.github.io/Bookmarklets/MSA-inject-grades.js';})();
---
+++ MSA-inject-grades.js
@@ -0,0 +1,11 @@
+function inject() {
+  $("#showGrade").click();
+}
+
+$( document ).ajaxComplete(function( event, xhr, settings ) {
+  if (settings.url.startsWith("/api/gradebook/GradeBookMyDayMarkingPeriods")){
+    setTimeout(inject, 500);
+  }
+});
+
+inject();
